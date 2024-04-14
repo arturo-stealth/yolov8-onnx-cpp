cb89813a61a447c9e63bf957368656b5f86a5bab
Jack Zender <jack@sliprobotics.com>
2024-04-14 07:08:43 +0000

    revert masks fn

--- include/yolov8_onnxruntime/utils/viz.h
+++ include/yolov8_onnxruntime/utils/viz.h
@@ -82,74 +82,53 @@ inline std::vector<cv::Scalar> generateRandomColors(int class_names_num, int num
   return colors;
 }
 
-inline void plot_masks(cv::Mat img,
-                       std::vector<YoloResults>& result,
-                       std::vector<cv::Scalar> color,
-                       std::unordered_map<int, std::string>& names)
+inline
+void plot_masks(cv::Mat img, std::vector<YoloResults>& result, std::vector<cv::Scalar> color,
+    std::unordered_map<int, std::string>& names)
 {
-  cv::Mat mask = img.clone();
-  for (int i = 0; i < result.size(); i++)
-  {
-    float left, top;
-    left = result[i].bbox.x;
-    top = result[i].bbox.y;
-    int color_num = i;
-    int& class_idx = result[i].class_idx;
-    cv::rectangle(img, result[i].bbox, color[result[i].class_idx], 2);
-
-    // try to get string value corresponding to given class_idx
-    std::string class_name;
-    auto it = names.find(class_idx);
-    if (it != names.end())
+    cv::Mat mask = img.clone();
+    for (int i = 0; i < result.size(); i++)
     {
-      class_name = it->second;
+        float left, top;
+        left = result[i].bbox.x;
+        top = result[i].bbox.y;
+        int color_num = i;
+        int& class_idx = result[i].class_idx;
+        rectangle(img, result[i].bbox, color[result[i].class_idx], 2);
+
+        // try to get string value corresponding to given class_idx
+        std::string class_name;
+        auto it = names.find(class_idx);
+        if (it != names.end()) {
+            class_name = it->second;
+        }
+        else {
+            std::cerr << "Warning: class_idx not found in names for class_idx = " << class_idx << std::endl;
+            // then convert it to string anyway
+            class_name = std::to_string(class_idx);
+        }
+
+        if (result[i].mask.rows && result[i].mask.cols > 0)
+        {
+            mask(result[i].bbox).setTo(color[result[i].class_idx], result[i].mask);
+        }
+        std::stringstream labelStream;
+        labelStream << class_name << " " << std::fixed << std::setprecision(2) << result[i].conf;
+        std::string label = labelStream.str();
+
+    	cv::Size text_size = cv::getTextSize(label, cv::FONT_HERSHEY_SIMPLEX, 0.6, 2, nullptr);
+        cv::Rect rect_to_fill(left - 1, top - text_size.height - 5, text_size.width + 2, text_size.height + 5);
+        cv::Scalar text_color = cv::Scalar(255.0, 255.0, 255.0);
+        rectangle(img, rect_to_fill, color[result[i].class_idx], -1);
+
+        putText(img, label, cv::Point(left - 1.5, top - 2.5), cv::FONT_HERSHEY_SIMPLEX, 0.6, text_color, 2);
     }
-    else
-    {
-      std::cerr << "Warning: class_idx not found in names for class_idx = " << class_idx
-                << std::endl;
-      // then convert it to string anyway
-      class_name = std::to_string(class_idx);
-    }
-
-    // if (result[i].mask.rows && result[i].mask.cols > 0)
-    // {
-    //     mask(result[i].bbox).setTo(color[result[i].class_idx], result[i].mask);
-    // }
-    if (result[i].mask.rows && result[i].mask.cols > 0)
-    {
-      // Shade the whole mask as before
-      mask(result[i].bbox).setTo(color[result[i].class_idx], result[i].mask);
-
-      // // Find contours in the mask
-      // std::vector<std::vector<cv::Point>> contours;
-      // cv::findContours(result[i].mask, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-
-      // // Draw the contours over the shaded area with a different color or opacity
-      // cv::Scalar boundaryColor = color[result[i].class_idx] * 0.5; // Example: Darker color
-      // // You can also define a completely different color here
-      // cv::drawContours(
-      //     mask(result[i].bbox), contours, -1, boundaryColor, 2); // Thickness of 2, adjust as
-      //     needed
-    }
-    std::stringstream labelStream;
-    labelStream << class_name << " " << std::fixed << std::setprecision(2) << result[i].conf;
-    std::string label = labelStream.str();
-
-    cv::Size text_size = cv::getTextSize(label, cv::FONT_HERSHEY_SIMPLEX, 0.6, 2, nullptr);
-    cv::Rect rect_to_fill(
-        left - 1, top - text_size.height - 5, text_size.width + 2, text_size.height + 5);
-    cv::Scalar text_color = cv::Scalar(255.0, 255.0, 255.0);
-    rectangle(img, rect_to_fill, color[result[i].class_idx], -1);
-
-    putText(
-        img, label, cv::Point(left - 1.5, top - 2.5), cv::FONT_HERSHEY_SIMPLEX, 0.6, text_color, 2);
-  }
-  addWeighted(img, 0.6, mask, 0.4, 0, img); // add mask to src
-  resize(img, img, img.size());
-  // imshow("img", img);
-  // cv::waitKey();
+    addWeighted(img, 0.6, mask, 0.4, 0, img); //add mask to src
+    resize(img, img, img.size());
+    // imshow("img", img);
+    // cv::waitKey();
 }
+
 
 // void plot_keypoints(cv::Mat& image, const std::vector<std::vector<float>>& keypoints, const
 // cv::Size& shape) {
